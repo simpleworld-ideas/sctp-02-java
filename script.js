c07dc5e5a7d8839e2db75e5d712a988e157a2ca6
simpleworld-ideas Akbar <akbarsuhaib10@gmail.com>
2024-02-04 04:52:15 +0000

    last

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function changeButton(){
-    document.querySelector("body").style.backgroundColor
-}
+// the DOMContentLoaded event happens on the document object
+// it triggers (i.e fires ) when all the DOM elements have been created
+// i.e wait for the webpage to be ready
+document.addEventListener("DOMContentLoaded", async function(){
+    const response = await axios.get("data.json");
+    const data = response.data;
+    console.log(data);
+    document.querySelector("#output").innerHTML=`
+    <h1>${data.Employee}</h1>
+    <p>Nmae: ${data.employees[0].name}</p>
+    <p>Credit Limit: ${data.valuation.credit_limt}</p>
+    `
+})
